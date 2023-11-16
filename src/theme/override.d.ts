a182f04ccd61e6d885e013d7b6d4357c517cdf85
Erick Christian <erickchristian48@gmail.com>
2023-11-17 06:55:17 +0700

    fix: colors typing

--- src/theme/override.d.ts
+++ src/theme/override.d.ts
@@ -1,7 +1,7 @@
-import { MantineColorsTuple } from '@mantine/core'
+import { DefaultMantineColor, MantineColorsTuple } from '@mantine/core'
 import { colors } from './colors'
 
-type ExtendedCustomColors = keyof typeof colors
+type ExtendedCustomColors = keyof typeof colors | DefaultMantineColor
 
 declare module '@mantine/core' {
   export interface MantineThemeColorsOverride {
