03501f2924f65eeaa84baadf1f52e5cfb5ed12b3
Erick Christian <erickchristian48@gmail.com>
2023-11-16 18:15:41 +0700

    chore: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { MantineProvider, Title } from "@mantine/core";
-import { theme } from "./theme";
+import { MantineProvider, Title } from '@mantine/core'
+import { theme } from './theme'
 // Fonts
-import "@fontsource-variable/plus-jakarta-sans";
+import '@fontsource-variable/plus-jakarta-sans'
 // CSS
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css'
 
 function App() {
   return (
     <MantineProvider theme={theme}>
       <Title>er-Todo</Title>
     </MantineProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,8 +1,8 @@
-import { createTheme } from "@mantine/core";
+import { createTheme } from '@mantine/core'
 
 export const theme = createTheme({
-  fontFamily: "Plus Jakarta Sans Variable, sans-serif",
+  fontFamily: 'Plus Jakarta Sans Variable, sans-serif',
   headings: {
-    fontWeight: "400",
+    fontWeight: '400',
   },
-});
+})
