ac774feee3fdf749217063f46a97e1b907c00f7b
Erick Christian <erickchristian48@gmail.com>
2023-11-17 06:55:36 +0700

    fix: use lf instead of crlf

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,13 +1,21 @@
-import { Button, Title, useMantineColorScheme } from '@mantine/core'
+import {
+  Box,
+  Button,
+  Center,
+  Title,
+  useMantineColorScheme,
+} from '@mantine/core'
 
 export const Todo = () => {
   const { toggleColorScheme } = useMantineColorScheme()
 
   return (
-    <>
+    <Center mih="100vh" w="100vw" style={{ flexDirection: 'column' }}>
       <Title>er-Todo</Title>
 
       <Button onClick={toggleColorScheme}>Button</Button>
-    </>
+
+      <Box w="min(400px, 100%)" bg="sunrise-yellow" mih="200px"></Box>
+    </Center>
   )
 }
